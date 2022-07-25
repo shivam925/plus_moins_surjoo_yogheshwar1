d39ebb31b16217f27acf55a5613b3d171207e984
shivam925 <shivamsurjoo17@gmail.com>
2022-07-25 15:02:02 +0400

    Level of difficulties
    If you insert 1, easy level
    If you insert 2, medium level
    If you insert 3, hard level

--- plus_moins.c
+++ plus_moins.c
@@ -1,64 +1,67 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void choix_joueur(int N)
+#include<stdio.h>
+#include<stdlib.h>
+int plus_moins(int choix_joueur, int valeur_a_trouver)
 {
-	int valeur_a_trouver, choix_joueur, nombre_de_devinettes = 0;
-
-
-	srand(time(NULL));
-
-
-	valeur_a_trouver = rand() % N;
-
-	printf("Devinez un nombre entre"
-		" 1 et %d\n",
-		N);
-
-
-	do {
-
-		if (nombre_de_devinettes > 20) {
-			printf("\nVous perdez!\n");
-			break;
-		}
-
-
-		scanf("%d", &choix_joueur);
-
-
-		if (choix_joueur > valeur_a_trouver)
-
-		{
-			printf("Moins\n");
-			nombre_de_devinettes++;
-		}
-
-		else if (valeur_a_trouver > choix_joueur)
-
-		{
-			printf("Plus\n");
-			nombre_de_devinettes++;
-		}
-
-		else
-			printf("Vous avez deviné le"
-				" nombre dans %d "
-				"tentatives!\n",
-				nombre_de_devinettes);
-
-	} while (choix_joueur != valeur_a_trouver);
+	if(choix_joueur < valeur_a_trouver)
+	{
+		printf("Plus\n");
+		return 0;
+	}
+	if(choix_joueur > valeur_a_trouver)
+	{
+		printf("Moins\n");
+		return 1;
+	}
+	if(choix_joueur == valeur_a_trouver)
+	{
+		printf("Bingo\n");
+		return 2;
+	}
 }
-
-
 int main()
 {
-	int N = 100;
-
-
-	choix_joueur(N);
-
-	return 0;
+	int valeur_a_trouver = rand() % 100 + 1;
+	int choix_joueur;
+	int resultat;
+	int mode;
+	int i = 0;
+	printf("À quel point aimeriez-vous que le jeu soit difficile?:\n");
+	printf("Insérer 1 pour facile\n");
+	printf("Insérer 2 pour moyen\n");
+	printf("Insérer 3 pour difficile\n");
+	scanf("%d",&mode);
+	if(mode == 1)
+	{
+		printf("Pour trouver le nombre caché, insérez un nombre entier: ");
+		scanf("%d",&choix_joueur);
+		resultat = plus_moins(choix_joueur,valeur_a_trouver);
+	}while(resultat<=1);
+		do
+		{
+			printf("Pour trouver le nombre caché, insérez un nombre entier: ");
+			scanf("%d",&choix_joueur);
+			resultat = plus_moins(choix_joueur,valeur_a_trouver);
+		i = i + 1;
+		}while(i<=1);
+	
+	if(mode == 2)
+	{
+		do
+                {
+                        printf("Pour trouver le nombre caché, insérez un nombre entier: ");
+                        scanf("%d",&choix_joueur);
+                        resultat = plus_moins(choix_joueur,valeur_a_trouver);
+			i = i + 1;
+                }while(i<25);
+	}
+	if(mode == 3)
+	{
+		do
+                {
+                        printf("Pour trouver le nombre caché, insérez un nombre entier: ");
+                        scanf("%d",&choix_joueur);
+                        resultat = plus_moins(choix_joueur,valeur_a_trouver);
+			i = i + 1;
+                }while(i<10);
+	}
 }
